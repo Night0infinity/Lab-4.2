abbe332235bc9a676dde71831502eace3ab1e49b
Night0infinity <l.kurylo2005@gmail.com>
2022-10-17 19:13:24 +0300

    first commit

---
+++ Lab 4.2/pr1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(7) << "x" << " |"
+		<< setw(10) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = 2 + 6 * x;
+		if (x <= 0)
+			B = log(cos(x)) + pow(x, 5);
+		else
+			if (x > 3)
+				B = 12*x - pow(x, 8);
+			else
+				//ctg = 1/tg
+				B = 1/tan((1+log(x))/3);
+		
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
